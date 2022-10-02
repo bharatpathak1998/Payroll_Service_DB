d2aacba7306194282e302776bb17b98bfbea12ba
bharatpathak1998 <bharatpathak099@gmail.com>
2022-10-02 16:53:58 +0530

    Added : Functionality To Create A Employee Payroll Table

--- Payroll_Service.sql
+++ Payroll_Service.sql
@@ -1,3 +1,13 @@
 create database payroll_service;
 show databases;
 Use payroll_service;
+
+create table employee_payroll
+(
+Id int not null auto_increment,
+Name varchar(20) not null,
+Salary int NOT NULL,
+Start date not null,
+primary key(Id)
+);
+desc employee_payroll;
