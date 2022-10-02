aec286a92d49ec073dec81fb505d8510b12dfec6
bharatpathak1998 <bharatpathak099@gmail.com>
2022-10-02 19:17:08 +0530

    Added : Functionality To Add Gender To Employee Payroll Table

--- Payroll_Service.sql
+++ Payroll_Service.sql
@@ -21,3 +21,12 @@ select * from employee_payroll;
 
 select Salary from employee_payroll where Name='Mohit';
 select Salary from employee_payroll where Start between CAST('2022-07-05' AS DATE) AND DATE(NOW());
+
+insert into employee_payroll (Name, Salary, Start) values
+('Aachal', 80000, '2022-05-10'),
+('Sonal', 80000, '2022-07-24');
+select * from employee_payroll;
+alter table employee_payroll add Gender char(1);
+update employee_payroll set Gender='F' where Name = 'Sonal' or Name='Aachal';
+update employee_payroll set Gender='M' where Name = 'Bharat' or Name='Yash' or Name='Mohit';
+select * from employee_payroll;
