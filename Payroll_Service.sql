1c5d5f31827b45c407463ab08015dccf6405f31d
bharatpathak1998 <bharatpathak099@gmail.com>
2022-10-02 20:01:24 +0530

    Added : Functionality To Extend Employee Payroll Data To Store Employee Phone, Address And Department

--- Payroll_Service.sql
+++ Payroll_Service.sql
@@ -42,3 +42,18 @@ select AVG(Salary) from employee_payroll where Gender='M' group by Gender;
 select MAX(Salary) from employee_payroll where Gender='M' group by Gender;
 select MIN(Salary) from employee_payroll where Gender='M' group by Gender;
 select COUNT(Id) from employee_payroll where Gender='M' group by Gender;
+
+alter table employee_payroll add PhoneNumber varchar(10);
+alter table employee_payroll add Address varchar(20) NOT NULL default 'Tumsar';
+alter table employee_payroll add Department varchar(20) NOT NULL default '';
+
+update employee_payroll set PhoneNumber='7559205894' where Name = 'Bharat';
+update employee_payroll set PhoneNumber='9158924884' where Name = 'Yash';
+update employee_payroll set PhoneNumber='9975481258' where Name='Mohit';
+update employee_payroll set PhoneNumber='7775892458' where Name='Sonal';
+update employee_payroll set PhoneNumber='7484464458' where Name='Aachal';
+
+update employee_payroll set Department='HR' where Name = 'Bharat' or Name='Yash';
+update employee_payroll set Department='Designing' where Name = 'Aachal' or Name = 'Sonal';
+update employee_payroll set Department='Marketing' where Name = 'Mohit';
+select * from employee_payroll;
